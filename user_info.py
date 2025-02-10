3c8285e685c8473484040e19fa73792c1a47e075
Milo2821 <victoremilio5510@gmail.com>
2025-02-10 11:38:03 -0600

    Add files via upload
    This helps to gather the information of a person and at the end of the inputs, the program generates the response telling the inputs in order

---
+++ user_info.py
@@ -0,0 +1,11 @@
+
+# Solicitar nombre, apellidos, edad, estatura y peso
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido_paterno = input("Por favor, ingresa tu apellido paterno: ")
+apellido_materno = input("Por favor, ingresa tu apellido materno: ")
+edad = input("Por favor, ingresa tu edad: ")
+estatura = input("Por favor, ingresa tu estatura en metros: ")
+peso = input("Por favor, ingresa tu peso en kilogramos: ")
+
+# Mostrar mensaje con la información recopilada
+print(f"Hola {nombre} {apellido_paterno} {apellido_materno}, tu edad es {edad} años, tienes una estatura de {estatura} m. y tu peso es de {peso} kg.")
