0e33797c3810c8d24d3932879e886b33892dff03
Milo2821 <victoremilio5510@gmail.com>
2025-02-11 23:17:42 -0600

    Add files via upload

---
+++ tarea2.2_385499.py
@@ -0,0 +1,16 @@
+# Solicitar al usuario 3 valores numéricos enteros
+valor1 = int(input("Ingrese el primer valor entero: "))
+valor2 = int(input("Ingrese el segundo valor entero: "))
+valor3 = int(input("Ingrese el tercer valor entero: "))
+
+# Realizar la suma de los 3 valores
+suma = valor1 + valor2 + valor3
+print(f"La suma de los valores es: {suma}")
+
+# Realizar la multiplicación de los 3 valores
+multiplicacion = valor1 * valor2 * valor3
+print(f"La multiplicación de los valores es: {multiplicacion}")
+
+# Realizar la resta de los 3 valores
+resta = valor1 - valor2 - valor3
+print(f"La resta de los valores es: {resta}")
